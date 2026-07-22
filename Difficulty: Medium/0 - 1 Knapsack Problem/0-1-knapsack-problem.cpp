e96fb3c0366172818ac64f6de342a140ce17e9ad
avisharma42 <156396607+avisharma42@users.noreply.github.com>
2026-07-22 22:46:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,28 +1,37 @@
 class Solution {
   public:
-  int t[1001][1001];
+  
   
     int solve(int W,int n, vector<int>& val,vector<int>& wt){
-      if(n==0 || W==0){
-          return 0;
-      }
-      if(t[n][W]!=-1){
-          return t[n][W];
-      }
-      if(W>=wt[n-1]){
-          t[n][W] = max(val[n-1]+solve(W-wt[n-1],n-1,val,wt),solve(W,n-1,val,wt));
-      }
-      else{
-          return t[n][W] = solve(W,n-1,val,wt);
-      }
-      return t[n][W];
+        //initializatrion
+        int t[n+1][W+1];
+        for(int i = 0; i<=n ; i++){
+            for(int j  = 0 ; j<=W ; j++){
+                if(i==0||j==0){
+                    t[i][j] = 0;
+                }
+            }
+        }
+        
+        //recusrsive calls to the iterations
+        for(int i = 1; i<=n ; i++){
+            for(int j = 1 ; j<=W ; j++){
+                if(wt[i-1]<=j){
+                    t[i][j] = max(val[i-1]+t[i-1][j-wt[i-1]] , t[i-1][j]);
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+            
+        }
+        return t[n][W];
+     
   }
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         // code here
-      
-        memset(t,-1,sizeof(t));
         int n = val.size();
-     int ans = solve(W,n,val,wt);
-     return ans;
+        return solve(W,n,val,wt);
+      
     }
 };

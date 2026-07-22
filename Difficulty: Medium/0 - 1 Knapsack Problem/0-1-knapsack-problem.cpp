0f18833ca51a2a820383b834dbd16d6965ecf9ee
avisharma42 <156396607+avisharma42@users.noreply.github.com>
2026-07-23 05:12:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,37 +1,32 @@
 class Solution {
   public:
-  
-  
-    int solve(int W,int n, vector<int>& val,vector<int>& wt){
-        //initializatrion
-        int t[n+1][W+1];
-        for(int i = 0; i<=n ; i++){
-            for(int j  = 0 ; j<=W ; j++){
-                if(i==0||j==0){
-                    t[i][j] = 0;
-                }
-            }
-        }
-        
-        //recusrsive calls to the iterations
-        for(int i = 1; i<=n ; i++){
-            for(int j = 1 ; j<=W ; j++){
-                if(wt[i-1]<=j){
-                    t[i][j] = max(val[i-1]+t[i-1][j-wt[i-1]] , t[i-1][j]);
-                }
-                else{
-                    t[i][j] = t[i-1][j];
-                }
-            }
-            
-        }
-        return t[n][W];
-     
+  int solve(int W, vector<int> &val, vector<int> &wt,int n){
+      int t[n+1][W+1];
+      //base condition
+      for(int i = 0 ; i <=n ; i++){
+          for(int j = 0 ;j<=W ; j++){
+              if(i==0 || j==0){
+                  t[i][j]=0;
+              }
+          }
+      }
+      //case daigram
+      for(int i = 1 ; i <=n ; i++){
+          for(int j = 1 ;j<=W ; j++){
+              if(wt[i-1]<=j){
+        t[i][j]=max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1][j]);
+              }
+              else{
+        t[i][j] = t[i-1][j];
+              }
+          }
+      }
+      return t[n][W];
   }
+  
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         // code here
-        int n = val.size();
-        return solve(W,n,val,wt);
-      
+     int n = val.size();
+     return solve(W,val,wt,n);
     }
 };

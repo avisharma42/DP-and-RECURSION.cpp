b7011a4655d0287c904bdb756b33cce89c8a6682
avisharma42 <156396607+avisharma42@users.noreply.github.com>
2026-07-27 22:48:14 +0530

    Time: 31 ms (38.26%) | Memory: 27.6 MB (16.48%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
+   int solve(string text1 , string text2 ,int n , int m){
+    vector<vector<int>> t(n+1,vector<int>(m+1));
 
-int solve(string text1, string text2,int n , int m){
-    //this is the base case
-  vector<vector<int>> t(n+1,vector<int>(m+1));
-    
-     for(int  i = 0 ; i<=n ; i++){
-        for(int j = 0 ; j<=m ; j++){
-           if(i==0||j==0){
+    //basse case
+    for(int i = 0; i<=n; i++){
+       for(int j = 0 ; j <=m ; j++){
+        if(i==0 || j==0){
             t[i][j]=0;
-           }
         }
-     }
-    //choice daigram
-      for(int  i = 1 ; i<=n ; i++){
-        for(int j = 1 ; j<=m ; j++){
-           if(text1[i-1]==text2[j-1]){
-            t[i][j]= 1+t[i-1][j-1];
-           }
-           else{
+       }
+    }
+        for(int i = 1; i<=n; i++){
+       for(int j = 1 ; j <=m ; j++){
+        if(text1[i-1]==text2[j-1]){
+           t[i][j] = 1+t[i-1][j-1];
+
+        }
+        else{
             t[i][j] = max(t[i-1][j],t[i][j-1]);
-           }
         }
-     }
+       }
+    }
     return t[n][m];
-}
+   }
+
     int longestCommonSubsequence(string text1, string text2) {
      
-        int n = text1.size();
-        int m =  text2.size();
-       
-        return solve(text1,text2,n , m);
+      int n = text1.size();
+      int m = text2.size();
+      return solve(text1 ,text2 ,n,m);
     }
 };
